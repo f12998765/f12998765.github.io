52b2011d636b13e7d431214d67e290d604bcc66a
XIZERO <f12998765@yeah.net>
2017-01-02 15:19:53 +0800

    2017 加油 🚀

--- js/top.js
+++ js/top.js
@@ -8,11 +8,20 @@ function goTop()
             $("#gotop").fadeOut(500);//以1秒的间隔渐隐id=gotop的元素
     });
 };
+
 $(function(){
     //点击回到顶部的元素
     $("#gotop").click(function(e) {
             //以1秒的间隔返回顶部
             $('body,html').animate({scrollTop:0},500);
+    });
+    $("#gotop").mouseover(function(e) {
+        $(this).css("background","url({{ .Site.BaseURL }}/img/z.png) no-repeat");
+        $(this).css("background-size","100% 100%");
+    });
+    $("#gotop").mouseout(function(e) {
+        $(this).css("background","url({{ .Site.BaseURL }}/img/z.png) no-repeat");
+        $(this).css("background-size","100% 100%");
     });
     goTop();//实现回到顶部元素的渐显与渐隐
 });
